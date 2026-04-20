4e064a3d6e9df0ab71f4811a66367681c2c6c5a5
Jesús Villena <jevigomis@gmail.com>
2026-04-20 22:17:49 +0200

    48775415M CADJugador cabeceras métodos

---
+++ hada-ProyectoGrupo.Library/CAD/CADJugador.cs
@@ -0,0 +1,41 @@
+﻿using hada_ProyectoGrupo.Library.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hada_ProyectoGrupo.Library.CAD
+{
+    internal class CADJugador
+    {
+
+        public CADJugador() { 
+        
+        }
+
+        public bool Create(ENJugador en)
+        {
+            bool ok = true;
+
+
+            return ok;
+        }
+
+        public bool Read(ENJugador en)
+        {
+            bool ok = true;
+
+
+            return ok;
+        }
+
+        public bool Update(ENJugador en)
+        {
+            bool ok = true;
+
+
+            return ok;
+        }
+    }
+}
---
+++ hada-ProyectoGrupo.Library/CAD/CADUsuario.cs
@@ -0,0 +1,48 @@
+﻿using hada_ProyectoGrupo.Library.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hada_ProyectoGrupo.Library.CAD
+{
+    internal class CADUsuario
+    {
+        public CADUsuario() { 
+        
+        }
+
+        public bool Login(ENUsuario en)
+        {
+            bool ok = true; // para que no de errror (se implementará correctamente en el futuro)
+
+
+            return ok;
+        }
+
+        public bool Create(ENUsuario en)
+        {
+            bool ok = true; 
+
+
+            return ok;
+        }
+
+        public bool Update(ENUsuario en)
+        {
+            bool ok = true; 
+
+
+            return ok;
+        }
+
+        public bool Delete(ENUsuario en)
+        {
+            bool ok = true; 
+
+
+            return ok;
+        }
+    }
+}
