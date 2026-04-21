3a047f7eb9c5800c4692da558b1471a266c11f9e
fn20 <fn20@alu.ua.es>
2026-04-21 22:09:13 +0200

    Agregar carpetas EN/CAD con sus archivos

---
+++ hada-ProyectoGrupo.Library/EN/NoticiaEN.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hada_ProyectoGrupo.Library.EN
+{
+    internal class NoticiaEN
+    {
+    }
+}
