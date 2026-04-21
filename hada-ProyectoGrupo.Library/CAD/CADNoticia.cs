161ed1a5cb7403f64c0bbe6581306ba90169e783
fn20 <fn20@alu.ua.es>
2026-04-22 00:01:49 +0200

    Z0289003J Añadiendo las clases ENNoticia, CADNoticia, ENPatrocinador y CADPatrocinador

---
+++ hada-ProyectoGrupo.Library/CAD/CADNoticia.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hada_ProyectoGrupo.Library.EN;
+
+namespace hada_ProyectoGrupo.Library.CAD
+{
+    public class CADNoticia
+    {
+        public CADNoticia() { }
+
+        public bool Create(ENNoticia en)
+        {
+            bool ok = true; // se implementará correctamente en el futuro
+
+            return ok;
+        }
+
+        public bool Read(ENNoticia en)
+        {
+            bool ok = true;
+
+            return ok;
+        }
+
+        public bool Update(ENNoticia en)
+        {
+            bool ok = true;
+
+            return ok;
+        }
+
+        public bool Delete(ENNoticia en)
+        {
+            bool ok = true;
+
+            return ok;
+        }
+
+        public List<ENNoticia> ReadAll()
+        {
+            List<ENNoticia> lista = new List<ENNoticia>(); // se implementará correctamente en el futuro
+
+            return lista;
+        }
+
+    }
+}
