e7f31e1d5adc5959c3c9ac451758984f613536bb
fn20 <fn20@alu.ua.es>
2026-04-24 11:57:48 +0200

    Z0289003J Implementando el registro

--- Default.aspx
+++ Default.aspx
@@ -1,10 +1,9 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="hada_ProyectoGrupo.Default" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <p>
-    <asp:LinkButton ID="LinkButton1" runat="server" PostBackUrl="~/Public/Login.aspx">Login</asp:LinkButton>
-    </p>
-    <asp:LinkButton ID="LinkButton2" runat="server" PostBackUrl="~/Public/Registro.aspx">Registro</asp:LinkButton>
 
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Bienvenido a HADA Esports</h1>
+    <p>Plataforma de torneos de videojuegos. Regístrate para participar.</p>
 </asp:Content>
--- Public/Registro.aspx.cs
+++ Public/Registro.aspx.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿using hada_ProyectoGrupo.Library.EN;
+using System;
 
 namespace hada_ProyectoGrupo.Public
 {
@@ -11,7 +7,43 @@ namespace hada_ProyectoGrupo.Public
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        protected void btnRegistro_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ENUsuario usuario = new ENUsuario(
+                    txtEmail.Text,
+                    txtPassword.Text,
+                    txtNombre.Text,
+                    DateTime.Parse(txtFecha.Text)
+                );
+                usuario.Apellidos = txtApellidos.Text;
+                usuario.Pais = txtPais.Text;
+
+                ENJugador jugador = new ENJugador(
+                    txtEmail.Text,
+                    txtApodo.Text
+                );
+
+                bool okUsuario = usuario.Register();
+                bool okJugador = jugador.Create();
+
+                if (okUsuario && okJugador)
+                {
+                    lblMensaje.ForeColor = System.Drawing.Color.Green;
+                    lblMensaje.Text = "Registro exitoso. Ya puedes iniciar sesión.";
+                }
+                else
+                {
+                    lblMensaje.Text = "Error al registrar. Inténtalo de nuevo.";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = "Error: " + ex.Message;
+            }
         }
     }
 }
