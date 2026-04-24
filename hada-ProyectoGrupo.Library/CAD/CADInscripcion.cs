17e1aea9976d899fe2a6ba3b28a5fa171473103e
Maksim <maksgrines@gmail.com>
2026-04-24 12:39:11 +0200

    completadoo CADInscripcion

--- hada-ProyectoGrupo.Library/CAD/CADInscripcion.cs
+++ hada-ProyectoGrupo.Library/CAD/CADInscripcion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using hada_ProyectoGrupo.Library.EN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,41 @@ using System.Threading.Tasks;
 
 namespace hada_ProyectoGrupo.Library.CAD
 {
-    internal class CADInscripcion
+    public class CADInscripcion
     {
+        public CADInscripcion()
+        {
+        }
+
+        public bool Create(ENInscripcion en)
+        {
+            bool ok = true;
+            return ok;
+        }
+
+        public bool Read(ENInscripcion en)
+        {
+            bool ok = true;
+            return ok;
+        }
+
+        public bool Update(ENInscripcion en)
+        {
+            bool ok = true;
+            return ok;
+        }
+
+        public bool Delete(ENInscripcion en)
+        {
+            bool ok = true;
+            return ok;
+        }
+
+        // Nuevo método siguiendo el estilo de CADTorneo
+        public List<ENInscripcion> ReadAll()
+        {
+            List<ENInscripcion> lista = new List<ENInscripcion>();
+            return lista;
+        }
     }
 }
