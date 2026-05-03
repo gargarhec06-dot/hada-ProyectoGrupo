97602f2ca01c10ed93eef711c5d6e51c68ed7c91
Jesús Villena <jevigomis@gmail.com>
2026-05-03 19:57:18 +0200

    48775415M CADTorneo implementado

--- hada-ProyectoGrupo.Library/CAD/CADTorneo.cs
+++ hada-ProyectoGrupo.Library/CAD/CADTorneo.cs
@@ -1,67 +1,146 @@
 ﻿using hada_ProyectoGrupo.Library.EN;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace hada_ProyectoGrupo.Library.CAD
 {
     public class CADTorneo
     {
+        private string s;
+
         public CADTorneo()
         {
-
+            s = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
         }
 
         public bool Create(ENTorneo en)
         {
-            bool ok = true;
-
-
+            bool ok = false;
+            SqlConnection c = new SqlConnection(s);
+            try
+            {
+                c.Open();
+                string query = "INSERT INTO Torneo (codigo, id_videojuego, precioInscripcion, nombre, descripcion, profesional, costeOrganizacion, fecha, ubicacion) VALUES (@cod, @vid, @pre, @nom, @des, @pro, @cos, @fec, @ubi)";
+                SqlCommand com = new SqlCommand(query, c);
+                com.Parameters.AddWithValue("@cod", en.Codigo);
+                com.Parameters.AddWithValue("@vid", en.IdVideojuego);
+                com.Parameters.AddWithValue("@pre", en.PrecioInscripcion);
+                com.Parameters.AddWithValue("@nom", en.Nombre);
+                com.Parameters.AddWithValue("@des", en.Descripcion);
+                com.Parameters.AddWithValue("@pro", en.Profesional);
+                com.Parameters.AddWithValue("@cos", en.CosteOrganizacion);
+                com.Parameters.AddWithValue("@fec", en.Fecha);
+                com.Parameters.AddWithValue("@ubi", en.Ubicacion);
+                if (com.ExecuteNonQuery() > 0) ok = true;
+            }
+            catch (Exception) { ok = false; }
+            finally { c.Close(); }
             return ok;
         }
 
         public bool Read(ENTorneo en)
         {
-            bool ok = true;
-
-
+            bool ok = false;
+            SqlConnection c = new SqlConnection(s);
+            try
+            {
+                c.Open();
+                string query = "SELECT * FROM Torneo WHERE codigo = @cod";
+                SqlCommand com = new SqlCommand(query, c);
+                com.Parameters.AddWithValue("@cod", en.Codigo);
+                SqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    en.IdVideojuego = (int)dr["id_videojuego"];
+                    en.PrecioInscripcion = float.Parse(dr["precioInscripcion"].ToString());
+                    en.Nombre = dr["nombre"].ToString();
+                    en.Descripcion = dr["descripcion"].ToString();
+                    en.Profesional = (bool)dr["profesional"];
+                    en.CosteOrganizacion = float.Parse(dr["costeOrganizacion"].ToString());
+                    en.Fecha = (DateTime)dr["fecha"];
+                    en.Ubicacion = dr["ubicacion"].ToString();
+                    ok = true;
+                }
+                dr.Close();
+            }
+            catch (Exception) { ok = false; }
+            finally { c.Close(); }
             return ok;
         }
 
         public bool Update(ENTorneo en)
         {
-            bool ok = true;
-
-
+            bool ok = false;
+            SqlConnection c = new SqlConnection(s);
+            try
+            {
+                c.Open();
+                string query = "UPDATE Torneo SET id_videojuego=@vid, precioInscripcion=@pre, nombre=@nom, descripcion=@des, profesional=@pro, costeOrganizacion=@cos, fecha=@fec, ubicacion=@ubi WHERE codigo=@cod";
+                SqlCommand com = new SqlCommand(query, c);
+                com.Parameters.AddWithValue("@cod", en.Codigo);
+                com.Parameters.AddWithValue("@vid", en.IdVideojuego);
+                com.Parameters.AddWithValue("@pre", en.PrecioInscripcion);
+                com.Parameters.AddWithValue("@nom", en.Nombre);
+                com.Parameters.AddWithValue("@des", en.Descripcion);
+                com.Parameters.AddWithValue("@pro", en.Profesional);
+                com.Parameters.AddWithValue("@cos", en.CosteOrganizacion);
+                com.Parameters.AddWithValue("@fec", en.Fecha);
+                com.Parameters.AddWithValue("@ubi", en.Ubicacion);
+                if (com.ExecuteNonQuery() > 0) ok = true;
+            }
+            catch (Exception) { ok = false; }
+            finally { c.Close(); }
             return ok;
         }
 
         public bool Delete(ENTorneo en)
         {
-            bool ok = true;
-
-
+            bool ok = false;
+            SqlConnection c = new SqlConnection(s);
+            try
+            {
+                c.Open();
+                string query = "DELETE FROM Torneo WHERE codigo = @cod";
+                SqlCommand com = new SqlCommand(query, c);
+                com.Parameters.AddWithValue("@cod", en.Codigo);
+                if (com.ExecuteNonQuery() > 0) ok = true;
+            }
+            catch (Exception) { ok = false; }
+            finally { c.Close(); }
             return ok;
         }
+
         public List<ENTorneo> ReadAll()
         {
-            List<ENTorneo> lista = new List<ENTorneo>
+            List<ENTorneo> lista = new List<ENTorneo>();
+            SqlConnection c = new SqlConnection(s);
+            try
             {
-                new ENTorneo(101, 1, 20.0f, "Valorant Cup 2026",
-                    "Torneo elite de estrategia.", true, 800.0f,
-                    new DateTime(2026, 5, 10), "online"),
-
-                new ENTorneo(102, 2, 5.0f, "FIFA championship",
-                    "Torneo abierto para todos.", false, 150.0f,
-                    new DateTime(2026, 5, 25), "Alicante"),
-
-                new ENTorneo(103, 1, 10.0f, "Torneo Invitacional",
-                    "Solo jugadores invitados.", true, 400.0f,
-                    new DateTime(2026, 6, 9), "Universidad de Alicante")
-            };
-
+                c.Open();
+                string query = "SELECT * FROM Torneo";
+                SqlCommand com = new SqlCommand(query, c);
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    ENTorneo en = new ENTorneo();
+                    en.Codigo = (int)dr["codigo"];
+                    en.IdVideojuego = (int)dr["id_videojuego"];
+                    en.PrecioInscripcion = float.Parse(dr["precioInscripcion"].ToString());
+                    en.Nombre = dr["nombre"].ToString();
+                    en.Descripcion = dr["descripcion"].ToString();
+                    en.Profesional = (bool)dr["profesional"];
+                    en.CosteOrganizacion = float.Parse(dr["costeOrganizacion"].ToString());
+                    en.Fecha = (DateTime)dr["fecha"];
+                    en.Ubicacion = dr["ubicacion"].ToString();
+                    lista.Add(en);
+                }
+                dr.Close();
+            }
+            catch (Exception) { }
+            finally { c.Close(); }
             return lista;
         }
     }
