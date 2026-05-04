6c1bdbda727117e2372ef350eefb3e02652431c8
Héctor García García <hgg21@mscloud.ua.es>
2026-05-04 18:52:29 +0200

    Arreglado Jugadores.cs prueba

--- Public/Jugadores.aspx.cs
+++ Public/Jugadores.aspx.cs
@@ -23,12 +23,12 @@ namespace hada_ProyectoGrupo.Public
         {
             List<ENJugador> lista = new List<ENJugador>
             {
-                new ENJugador(1, "ana.garcia@gmail.com", "MidMaster", "Mid Laner", 4.2f, 58.5f, 45, "PC Gaming - RTX 3080", true, 101),
-               new ENJugador(2, "carlos.lopez@hotmail.com", "TopGod", "Top Laner", 3.8f, 55.2f, 38, "Laptop Gaming - RTX 3060", false, 102),
-               new ENJugador(3, "lucia.martinez@gmail.com", "JungleQueen", "Jungler", 5.1f, 62.3f, 52, "PC Ultra - RTX 4090", true, 103),
-               new ENJugador(4, "mario.rodriguez@yahoo.com", "ADCPro", "AD Carry", 4.5f, 60.1f, 47, "PC Gaming - RTX 4070", false, 101),
-                new ENJugador(5, "elena.sanchez@gmail.com", "SupportLover", "Support", 3.5f, 53.7f, 41, "PC Standard - RTX 3060 Ti", true, 0),
-                new ENJugador(6, "david.fernandez@outlook.com", "FlexPlayer", "Fill", 4.0f, 56.8f, 35, "Laptop Gaming - RTX 3050", true, 104)
+                new ENJugador(1, "ana.garcia@gmail.com", "MidMaster", "Mid Laner", 4.2f, 58.5f, 45, "PC Gaming - RTX 3080", true, 101,3),
+               new ENJugador(2, "carlos.lopez@hotmail.com", "TopGod", "Top Laner", 3.8f, 55.2f, 38, "Laptop Gaming - RTX 3060", false, 102,2),
+               new ENJugador(3, "lucia.martinez@gmail.com", "JungleQueen", "Jungler", 5.1f, 62.3f, 52, "PC Ultra - RTX 4090", true, 103,1),
+               new ENJugador(4, "mario.rodriguez@yahoo.com", "ADCPro", "AD Carry", 4.5f, 60.1f, 47, "PC Gaming - RTX 4070", false, 101,8),
+                new ENJugador(5, "elena.sanchez@gmail.com", "SupportLover", "Support", 3.5f, 53.7f, 41, "PC Standard - RTX 3060 Ti", true, 0,9),
+                new ENJugador(6, "david.fernandez@outlook.com", "FlexPlayer", "Fill", 4.0f, 56.8f, 35, "Laptop Gaming - RTX 3050", true, 104,8)
             };
             rptJugadores.DataSource = lista;
             rptJugadores.DataBind();
